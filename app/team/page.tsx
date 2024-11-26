384bb683b0f4ab14b2603fc47d072053fb52e567
21 Tech Enterprise <59superrichsol@gmail.com>
2024-11-26 16:38:57 +0700

    done

--- app/team/page.tsx
+++ app/team/page.tsx
@@ -7,25 +7,9 @@ import { auth } from "@/server/auth";
 import { db } from "@/server/db";
 import { redirect } from "next/navigation";
 import { getRoleStatus } from "@/server/actions/get-role-status";
+import { User } from "@prisma/client";
 
-// export interface Team {
-//   isAdmin: boolean;
-//   name: string;
-//   image: string;
-//   isApproved: boolean;
-//   email: string;
-// }
-
-// async function getTeam(): Promise<Team[]> {
-//   const res = await fetch(
-//     "https://66a6d52223b29e17a1a39127.mockapi.io/team",
-//     { cache: "no-store" }
-//   );
-//   const data = await res.json();
-//   return data;
-// }
-
-export type Team = z.infer<typeof TeamSchema>;
+export type Team = User;
 
 export default async function page() {
   const team = await db.user.findMany({});
--- types/team-schema.ts
+++ types/team-schema.ts
@@ -1,10 +1,15 @@
 import * as z from "zod";
 
 export const TeamSchema = z.object({
+  id: z.string().optional(),
   email: z.string(),
   name: z.string().nullable().optional(),
   isAdmin: z.boolean().optional(),
   isApproved: z.boolean().optional(),
   image: z.string().nullable().optional(),
   location: z.string().nullable().optional(),
+  username: z.string().optional(),
+  hashedPassword: z.string().optional(),
+  createdAt: z.date().optional(),
+  updatedAt: z.date().optional(),
 });

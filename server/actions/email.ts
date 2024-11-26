53827ba59f05898984654b963161a0341d9dd8c6
21 Tech Enterprise <59superrichsol@gmail.com>
2024-11-26 18:56:27 +0700

    add email

--- server/actions/email.ts
+++ server/actions/email.ts
@@ -2,14 +2,14 @@
 
 import {Resend} from "resend"
 
-const loginDomain = "http://localhost:3000/login"
-const registerDomain = "http://localhost:3000/register"
+const loginDomain = "https://next-dashboard-tjtw.vercel.app/login"
+const registerDomain = "https://next-dashboard-tjtw.vercel.app/register"
 const resend = new Resend(process.env.RESEND_API_KEY)
 
 export const sendApprovalEmail = async (email: string,  name: string | null | undefined) => {
   const {data, error} = await resend.emails.send({
     from: "Acme <onboarding@resend.dev>",
-    to: "ar919061493@gmail.com",
+    to: "ronnakritcheinvichai@gmail.com",
     subject: "Your Nexa Dashboard Account has been Approved!",
     html: `<p>Hey ${name} your account has been approved! Please login here  <a href=${loginDomain}>Here</a></p>`
   })
@@ -20,7 +20,7 @@ export const sendApprovalEmail = async (email: string,  name: string | null | un
 export const sendDeclineEmail = async (email: string, name: string) => {
   const {data, error} = await resend.emails.send({
     from: "Acme <onboarding@resend.dev>",
-    to: "ar919061493@gmail.com",
+    to: "ronnakritcheinvichai@gmail.com",
     subject: "Your Nexa Dashboard Account has been Declined!",
     html: `<p>Hey ${name} unfortunately your account has been declined! If you think this is a mistake please register again <a href=${registerDomain}>Here</a></p>`
   })
